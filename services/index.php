99ae4252d8a888b342ae6b999a0d0219d0c8a3bf
Raaz7147 <79857182+Raaz7147@users.noreply.github.com>
2021-03-01 20:42:36 +0530

    Add files via upload

---
+++ services/index.php
@@ -0,0 +1,200 @@
+<!DOCKTYPE html>
+<html>
+    <head>
+        <title>
+           ASSIGN PROJECT 
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width",initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+         <link rel="stylesheet" href="css/bootstrap.min.css">                <!--Bootstrap.css-->
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">      <!--fontawosome-->
+                             
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+          <link rel="stylesheet" href="css/custom.css"> 
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-success fixed-top">  
+            <a href="index.php" class="navbar-brand"><b>TECHNO SERVICE CENTER</b></a>
+            <i class="fa fa-bug fa-5x" style="color:red;"></i>
+            <p align="center" margin="18px"class="font-italic"style="text-shadow:2px 2px 2px #f242f5;"><b><strong>COME AND JOIN THE HAPPINESS</strong></b></p>
+
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#mymenu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="mymenu">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="index.php" class="nav-link"> Home</a></li><br>  
+                    <li class="nav-item"><a href="#registration" class="nav-link"> Registration</a></li><br>
+                     <li class="nav-item"><a href="#Services" class="nav-link"> Services</a></li> <br>
+                    <li class="nav-item"><a href="#Contact" class="nav-link"> Contact us</a></li>    
+                </ul>
+            </div>
+        </nav>
+        
+        <div class="jumbotron back-image" style="background-image:url(images/intro.jpg);">
+            <div class="myclass" style="padding-top:280px;padding-left:700px;">
+                <h4 style="color:black;" class="text-uppercase"><strong><b>Welcome to our Service Center</b></strong></h4>
+                <p style="color:brown"><em>High Rated...</em></p>
+                <a class="btn btn-primary btn-sm">Login</a>
+                 <a class="btn btn-warning btn-sm">Sign up</a>
+            </div>   
+        </div>
+        
+        <div class="container">
+            <div class="jumbotron bg-danger">
+                <h3 style="text-align:center;color:red;"><b><u>Management</u></b></h3>
+                <p>We provide the best services in your city and 
+                    Good customer service typically means providing timely, attentive, 
+                    upbeat service to a customer, 
+                    and making sure their needs are met in a manner that reflects 
+                    positively on the company or business.</p>
+            </div>
+        </div>
+        
+        <div class="container text-center">
+            <h3 style="color:darkturquoise;" id="Services"><b><u>Our Services</u><b></h3>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <a href="#"><i class="fa fa-television fa-5x"aria-hidden="false"></i></a>
+                                <h4 class="mt-4">SELL</h4>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="#"><i class="fa fa-android fa-5x" text-sucess"></i></a>
+                                <h4 class="mt-4">REPAIR</h4>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="#"><i class="fa fa-cog fa-spin fa-xl fa-5x"></i></a>
+                                <h4>TOOLS</h4>
+                            </div>
+                        </div><br><br>
+                       
+                        <div class="container" id="registration">
+                            <h3 style="color:#c934eb;"><b><u>Create your Account</u></b></h3>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <form action="" method="POST">                    
+                <i class="fa fa-user"></i> User:<input type="text" name="name" placeholder="Enter ur name" class="form-control"><br>
+                <i class="fa fa-envelope-o"></i> E-MAIL:<input type="email" name="name" placeholder="Enter ur e-mail" class="form-control"><br>
+                <i class="fa fa-key"></i> PASSWORD:<input type="password" name="name" placeholder="Enter ur password" class="form-control">
+                   <small class="form-text">Note:- we will never share your password to anyone</small><br><br>
+               <button class="btn btn-danger btn-block font-weight-bold" type="submit" value="submit" name="submit">Submit</button>            
+                                </div>
+                            </div>
+           
+                  <div class="jumbotron bg-primary">
+                                <div class="container">
+                                    <h3 style="color:#761c19;"><b><u>Customer Reviews</u></b></h3><br>
+                                    <div class="row">
+                                        <div class="col-sm-3">
+           <img src="images/15.jpg" class="img-fluid" alt="customer1"style="width: 200px;border-radius:100px;">
+           <h4 style="color:brown;"><i>Anjali Bhardwaj</i></h4>
+           <small class="text-font" style="color:#ec971f">Banglore</small>
+           <p style="text-align:center; text-align:justify;font-size:20px;">
+               I owned a MI-3 phone which don't work well for slightly 2-weeks.Then 
+               i came to know about techno service center
+               i went there and told them to fixed and they do it and now my phone is good.
+           </p> 
+                                        </div>
+            <div class="col-sm-3">
+              <img src="images/16.jpg" class="img-fluid" alt="customer2"style="width: 200px;border-radius:100px;">
+               <h4 style="color:brown;"><i>Ayush jha</i></h4>
+               <small class="text-font" style="color:#ec971f">Kolkata</small>
+           <p style="text-align:center; text-align:justify; font-size:20px;">
+               I repaired my router in techno service center
+               there technician was very good and well qualified
+               in their field as result they fixed my router in 1-hr.   
+           </p> 
+           </div>       
+              <div class="col-sm-3">
+              <img src="images/22.jpg" class="img-fluid" alt="customer3"style="width: 200px;border-radius:100px;">
+               <h4 style="color:brown;"><i>Shubham kr Giri</i></h4>
+               <small class="text-font" style="color:#ec971f">Delhi</small>
+           <p style="text-align:center; text-align:justify; font-size:20px;">
+             I repaired my MSI  laptop there,the environment was very good
+             and technician behavior was also very good. I rate 9/10.    
+           </p> 
+           </div>               
+           <div class="col-sm-3">
+              <img src="images/20.jpg" class="img-fluid" alt="customer4"style="width: 200px;border-radius:100px;">
+               <h4 style="color:brown;"><i>Ankit Singh</i></h4>
+               <small class="text-font" style="color:#ec971f">Munbai</small>
+           <p style="text-align:center; text-align:justify; font-size:20px;">
+             first time i came to techno service center to repair my samsung phone,
+             technician took 1-day and don't know the reason may 
+             be my was heavy destory but they fixed my problem and still
+             my phone running well.
+           </p> 
+           </div>                                
+                                    </div>        
+                                </div>
+                        </div> 
+                            <div class="container" id="Contact">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <h4 style="color:#c7254e"><b><u>Contact Us</u></b></h4>
+                                        <form action="" method="POST">
+                                            <input type="text" class="form-control" name="name" placeholder="Name"><br>
+                                            <input type="text" class="form-control" name="name" placeholder="Subject"><br>
+                                            <input type="e-mail" class="form-control" name="name" placeholder="E-mail"><br>
+                                            <textarea class="form-control" name="message" placeholder="How can I help you?" style="height:150px;">    
+                                            </textarea><br>
+                                            <input type="submit" value="Send" class="btn btn-success"><br><br>
+                         </form>
+                                        <div class="jumbotron bg-primary">
+                                        <div class="container bg-danger">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <h4 style="color:#d43f3a;"><u>Delhi</u></h4>
+                                                    Rani-nagar(Krishna Apartment)<br>
+                                                    Sardar patel colony<br>
+                                                   Phone: +123456 , Pin-700102
+                                                    
+                                                </div>
+                                                  <div class="col-sm-4">
+                                                      <h4 style="color:#d43f3a;"><u>Kolkata</u></h4>
+                                                    Naya-patti(Midland park)<br>
+                                                    Silver-moon<br>
+                                                    Phone: +0001234 , Pin-700123
+                                                    
+                                                </div>
+                                                  <div class="col-sm-4">
+                                                      <h4 style="color:#d43f3a;"><u>Banglore</u></h4>
+                                                    IT-Hub colony(Jamuna Apartment)<br>
+                                                    Boring road<br>
+                                                    Phone: +1200450 , Pin-700112
+                                                    
+                                                </div>
+                                                
+                                            </div>
+                                        </div>              
+                                    </div>
+                                </div>
+                            </div> 
+                            </div><br>
+                            <nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-bottom">
+                           
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p style="padding-left:30px;margin-left:-200px;">Follow us:</p>
+                                        <a href="#" target="_blank"class="fi-color" style="color:#ac2925; margin-left:-180px;"><i class="fa fa-facebook"></i></a>  
+                                         <a href="#" target="_blank" class="fi-color" style="color:#ac2925;"><i class="fa fa-instagram"></i></a>
+                                          <a href="#" target="_blank" class="fi-color" style="color:#ac2925;"><i class="fa fa-twitter"></i></a>
+                                           <a href="#" target="_blank" class="fi-color" style="color:#ac2925;"><i class="fa fa-youtube"></i></a>
+                                        <a href="#" target="_blank" class="fi-color" style="color:#ac2925;"><i class="fa fa-telegram"></i></a>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p> designed by:<i><em>Prince Raj</em></i><p>
+                                               <p  style="color:#ac2925;"> All copy rights are reserved @2021tec</p>
+                                           </div>
+                                </div>
+                           
+                        </nav>   
+        </div>
+                        <script src="js/jquery.min.js"></script>
+                        <script src="popper.min.js"></script>
+                        <script src="bootstrap.min.js"></script>
+                    <!--    <script src="js/all.js"></script> -->
+                        
+    </body>
+</html>
